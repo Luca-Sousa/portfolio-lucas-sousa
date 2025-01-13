af938288f5f2a75852a6619b219b4c6eba244d30
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-01-13 14:16:41 -0300

    feat: add page login and update globals css

---
+++ app/login/page.tsx
@@ -0,0 +1,21 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../_lib/auth";
+import { redirect } from "next/navigation";
+import LoginForm from "../_components/login-form";
+
+const LoginPage = async () => {
+  const session = await getServerSession(authOptions);
+  if (session) {
+    redirect("/dashboard");
+  }
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
