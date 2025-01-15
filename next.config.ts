b0e120b476b5e745f3275cc0cb4c85ca6bba4d87
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-01-14 22:21:42 -0300

    chore: remove images domains and add images remotePatterns to nextconfig

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["files.edgestore.dev"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "files.edgestore.dev",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
