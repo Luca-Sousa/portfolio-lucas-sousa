d3276307be75624dc7dec839e6404358be02b491
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-01-21 09:20:51 -0300

    feat: update login form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../_lib/auth";
 import { redirect } from "next/navigation";
 import LoginForm from "./_components/login-form";
+import Image from "next/image";
 
 const LoginPage = async () => {
   const session = await getServerSession(authOptions);
@@ -10,8 +11,14 @@ const LoginPage = async () => {
   }
 
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-3xl">
+    <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <div className="flex items-center gap-2 self-center text-xl font-medium">
+          <div className="relative flex size-12 items-center justify-center overflow-hidden rounded-md bg-primary text-primary-foreground">
+            <Image alt="Logo" src="/logo.png" fill className="object-contain" />
+          </div>
+          Lucas Sousa
+        </div>
         <LoginForm />
       </div>
     </div>
